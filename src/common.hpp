6bcbcc66149eebdcfa6faaa5ff5cd7548722422e
Tomoyuki Fujimori <moyu@dromozoa.com>
2024-03-17 00:23:21 +0900

    ログの扱いを変更。

--- src/common.hpp
+++ src/common.hpp
@@ -5,17 +5,36 @@
 #include <iostream>
 #include <string>
 
-#define LOG(name) LOG_IMPL(#name)
-#define DLOG(name) LOG_IMPL(#name)
-#define CHECK(name) LOG_IMPL(#name)
+#define LOG_IMPL(name) stream_wrapper{&std::cout}
+#define LOG(name) LOG_IMPL(name)
+#define DLOG(name) LOG_IMPL(name)
+#define CHECK(name) LOG_IMPL(name)
 
-inline std::ostream& LOG_IMPL(const std::string& name) {
-  if (name == "INFO" || name == "ok") {
-    return std::cout;
-  } else {
-    return std::cerr;
+class stream_wrapper {
+public:
+  explicit stream_wrapper(std::ostream* stream = nullptr)
+    : stream_(stream) {}
+
+  stream_wrapper(const stream_wrapper&) = delete;
+  stream_wrapper& operator=(const stream_wrapper&) = delete;
+
+  ~stream_wrapper() {
+    if (stream_) {
+      *stream_ << std::endl;
+    }
   }
-}
+
+  template <class T>
+  stream_wrapper& operator<<(const T& that) {
+    if (stream_) {
+      *stream_ << that;
+    }
+    return *this;
+  }
+
+private:
+  std::ostream* stream_;
+};
 
 #define CHECK_NOTNULL(expr) assert((expr) != nullptr)
 #define CHECK_EQ(a, b) assert((a) == (b))
